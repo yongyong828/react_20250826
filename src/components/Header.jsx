9331d39bb1f438f720f90192bec54789452e16ca
yongyong828 <minykimm12@gmail.com>
2026-01-23 12:12:33 +0900

    20260123 수업
    20260123 수업

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header id='header' role='banner'>
+            <div className="header_inner">
+                <h1 className='header_logo'>
+                    portfolio
+                    <em>developer</em>
+                </h1>
+                <nav className='header_nav show' role='navigation' aria-label='메인메뉴'>
+                    <ul>
+                        <li><a href="/">Intro</a></li>
+                        <li><a href="/">Skill</a></li>
+                        <li><a href="/">Site</a></li>
+                        <li><a href="/">Port</a></li>
+                        <li><a href="/">Contact</a></li>
+                    </ul>
+                </nav>
+                <nav className='header_nav_mobile' id='headerToggle' role='button' tabIndex='0'>
+                    <span></span> {/*햄버거메뉴 줄 하나 만들어야 하므로 span*/}
+                </nav>
+            </div>
+        </header>
     )
 }
 
--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Skip = () => {
     return (
-        <div>
-            Skip 영역입니다.
+        <div id='skip'>
+            <a href="#header">헤더영역 바로가기</a>
+            <a href="#intro">소개영역 바로가기</a>
+            <a href="#skill">스킬영역 바로가기</a>
+            <a href="#site">사이트영역 바로가기</a>
+            <a href="#port">포트폴리오영역 바로가기</a>
+            <a href="#contact">연락처영역 바로가기</a>
+            <a href="#footer">푸터영역 바로가기</a>
         </div>
     )
 }
