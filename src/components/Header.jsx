f4b82be3b05343c974ab6d04a18887b48a9ed740
yongyong828 <minykimm12@gmail.com>
2026-01-27 09:41:05 +0900

    skill 까지
    skill 까지

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const headerNav=[
+    {
+        title:"Intro",
+        url:"#Intro"
+    },
+    {
+        title:"Skill",
+        url:"#Skill"
+    },
+    {
+        title:"Site",
+        url:"#Site"
+    },    
+    {
+        title:"Port",
+        url:"#Port"
+    },   
+    {
+        title:"Contact",
+        url:"#Contact"
+    },   
+
+]
 
 const Header = () => {
+
+    const [show,setShow] = useState(false);
+    const toggleMenu = ()=>{
+        setShow(pshow => !pshow)
+        //(pshow => !pshow) : 참 -> 거짓, 거짓 -> 참으로 변경
+        //! : 반대값으로 변경
+    }
+    //show : 상태를 만들고 초기값을 false으로 지정
+    // setShow : 상태값을 바꾸는 값
+
+    //useState : 상태를 만들어주는 HOOK명령어
+    //HOOK명령어 : 컴포넌트 안에서 상태를 만들고 관리하는 명령어들.
+
     return (
         <header id='header' role='banner'>
             <div className="header_inner">
@@ -8,16 +45,14 @@ const Header = () => {
                     portfolio
                     <em>developer</em>
                 </h1>
-                <nav className='header_nav show' role='navigation' aria-label='메인메뉴'>
+                <nav className={`header_nav ${show ? "show":""}`} role='navigation' aria-label='메인메뉴'>
                     <ul>
-                        <li><a href="/">Intro</a></li>
-                        <li><a href="/">Skill</a></li>
-                        <li><a href="/">Site</a></li>
-                        <li><a href="/">Port</a></li>
-                        <li><a href="/">Contact</a></li>
+                        {headerNav.map((nav,key)=>(
+                            <li key={key}><a href={nav.url}>{nav.title}</a></li>
+                        ))}
                     </ul>
                 </nav>
-                <nav className='header_nav_mobile' id='headerToggle' role='button' tabIndex='0'>
+                <nav className='header_nav_mobile' id='headerToggle' role='button' tabIndex='0' onClick={toggleMenu}>
                     <span></span> {/*햄버거메뉴 줄 하나 만들어야 하므로 span*/}
                 </nav>
             </div>
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
+import about from "../assets/img/intro.png"
+
+const introText={
+    title:"port developer",
+    desc:["talent is","found at the end of the","effort"]
+}
 
 const Intro = () => {
     return (
-        <div>
-            Intro
-        </div>
+        <section id='intro'>
+            <div className="intro_inner">
+                <div className="intro_title">
+                    {introText.title}
+                    {/* port developer */}
+                </div>
+                <div className="intro_lines" aria-hidden="true">
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                </div>
+                <div className="intro_text">
+                    <div className="text">
+                        <div>{introText.desc[0]}</div>
+                        <div>{introText.desc[1]}</div>
+                        <div>{introText.desc[2]}</div>
+                    </div>
+                    <div className="img">
+                        <img src={about} alt="intro 이미지" />
+                    </div>
+                </div>
+                <div className="intro_lines bottom" aria-hidden="true">
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                    <span className='line'></span>
+                </div>
+            </div>
+        </section>
     )
 }
 
