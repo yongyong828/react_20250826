6bda69268f316a5c45168ddcce0981d262778137
yongyong828 <minykimm12@gmail.com>
2026-01-27 14:18:53 +0900

    port 추가
    port 추가

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,10 +1,79 @@
 import React from 'react'
+import Port1 from "../assets/img/port_1.png";
+import Port2 from "../assets/img/port_2.png";
+import Port3 from "../assets/img/port_3.png";
+import Port4 from "../assets/img/port_4.png";
+import Port5 from "../assets/img/port_5.png";
+import Port6 from "../assets/img/port_6.png";
 
 const Port = () => {
     return (
-        <div>
-            Port
-        </div>
+        <section id='port'>
+            <div className="port_inner">
+                <div className="port_title">
+                    portfolio <em>작업물</em>
+                </div>
+                <div className="port_wrap">
+                    <article className='port_item p1'>
+                        <span className='num'>01.</span>
+                        <a href="/" target='_blank' className='img'>
+                            <img src={Port1} alt="포폴이미지1" />
+                        </a>
+                        <h3 className='title'>A팀 프로젝트</h3>
+                        <div className="desc">11111 Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, harum voluptate hic culpa exercitationem repudiandae adipisci voluptatem ratione dicta aspernatur odio maiores error obcaecati dolorum corrupti excepturi distinctio cum deserunt!</div>
+                        <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+
+                        {/* noreferrer : 정보 보안용 => 사용자가 링크를 클릭했을때 어디서 왔는지 정보가 전달되게 해주는 경로를 숨겨줌 */}
+                        {/* noopener : 보안용 -> 차단, 해킹, 피싱등을 방지해줌 */}
+                    </article>
+                    <article className='port_item p2'>
+                        <span className='num'>02.</span>
+                        <a href="/" target='_blank' className='img'>
+                            <img src={Port2} alt="포폴이미지2" />
+                        </a>
+                        <h3 className='title'>B팀 프로젝트</h3>
+                        <div className="desc">22222 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius iure id amet pariatur officiis fugit provident nemo, delectus non facere?</div>
+                        <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+                    </article>
+                    <article className='port_item p3'>
+                        <span className='num'>03.</span>
+                        <a href="/" target='_blank' className='img'>
+                            <img src={Port3} alt="포폴이미지3" />
+                        </a>
+                        <h3 className='title'>C팀 프로젝트</h3>
+                        <div className="desc">33333 Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, harum voluptate hic culpa exercitationem repudiandae adipisci voluptatem ratione dicta aspernatur odio maiores error obcaecati dolorum corrupti excepturi distinctio cum deserunt!</div>
+                        <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+                    </article>
+                    <article className='port_item p4'>
+                        <span className='num'>04.</span>
+                        <a href="/" target='_blank' className='img'>
+                            <img src={Port4} alt="포폴이미지4" />
+                        </a>
+                        <h3 className='title'>D팀 프로젝트</h3>
+                        <div className="desc">44444 Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus excepturi fugit omnis perferendis eligendi, quas deserunt dolores necessitatibus exercitationem dolorem?</div>
+                        <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+                    </article>
+                    <article className='port_item p5'>
+                        <span className='num'>05.</span>
+                        <a href="/" target='_blank' className='img'>
+                            <img src={Port5} alt="포폴이미지5" />
+                        </a>
+                        <h3 className='title'>E팀 프로젝트</h3>
+                        <div className="desc">55555 Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, harum voluptate hic culpa exercitationem repudiandae adipisci voluptatem ratione dicta aspernatur odio maiores error obcaecati dolorum corrupti excepturi distinctio cum deserunt!</div>
+                        <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+                    </article>
+                    <article className='port_item p6'>
+                        <span className='num'>06.</span>
+                        <a href="/" target='_blank' className='img'>
+                            <img src={Port6} alt="포폴이미지6" />
+                        </a>
+                        <h3 className='title'>F팀 프로젝트</h3>
+                        <div className="desc">66666 Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam alias iste, deserunt aut eius corrupti consequatur aperiam assumenda at officia?</div>
+                        <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+                    </article>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,10 +1,133 @@
 import React from 'react'
 
+const siteText=[
+    {
+        text:["Make","site compliant with","mbc academy"],
+        title:"웹표준을 준수한 사이트 제작",
+        code:"/",
+        view:"/",
+        infor:["site coding","production period: two days","html5, css3, script"],
+    },
+    {
+        text:["Make","site compliant with","1차 포트폴리오"],
+        title:"1차 포트폴리오 사이트 제작",
+        code:"/",
+        view:"/",
+        infor:["site coding","production period: two days","html5, css3, script"],
+    },
+    {
+        text:["Make","site compliant with","팀프로젝트"],
+        title:"팀프로젝트 사이트 제작",
+        code:"/",
+        view:"/",
+        infor:["site coding","production period: two days","html5, css3, script"],
+    },
+    {
+        text:["Make","site compliant with","2차 포트폴리오"],
+        title:"2차 포트폴리오 사이트 제작",
+        code:"/",
+        view:"/",
+        infor:["site coding","production period: two days","html5, css3, script"],
+    },
+    {
+        text:["Make","site compliant with","리디자인"],
+        title:"리디자인 사이트 제작",
+        code:"/",
+        view:"/",
+        infor:["site coding","production period: two days","html5, css3, script"],
+    },
+]
+
 const Site = () => {
     return (
-        <div>
-            Site
-        </div>
+        <section id='site'>
+            <div className="site_inner">
+                <h2 className='site_title'>
+                    Site coding
+                    <em> 나의 작업물</em>
+                </h2>
+                <section className='site_wrap'>
+                    {siteText.map((site,key)=>(
+
+                        <article className={`site_item s${key+1}`} key={key}>
+                            <span className='num'>{key+1}.</span>
+                            <div className='text'>
+                                <div>{site.text[0]}</div>
+                                <div>{site.text[1]}</div>
+                                <div>{site.text[2]}</div>
+                            </div>
+                            <h3 className='title'>{site.title}</h3>
+                            <div className='btn'>
+                                <a href={site.code}>code</a> 
+                                <a href={site.view}>view</a>
+                            </div>
+                            <div className="infor">
+                                <span>{site.infor[0]}</span>
+                                <span>{site.infor[1]}</span>
+                                <span>{site.infor[2]}</span>
+                            </div>
+                        </article>
+
+                    ))}
+
+
+                    {/* <article className='site_item s2'>
+                        <span className='num'>2.</span>
+                        <div className='text'>
+                            <div>Make</div>
+                            <div>site compliant with</div>
+                            <div>1차 포트폴리오</div>
+                        </div>
+                        <h3 className='title'>1차 포트폴리오 사이트 제작</h3>
+                        <div className='btn'>
+                            <a href="/">code</a>
+                            <a href="/">view</a>
+                        </div>
+                        <div className="infor">
+                            <span>site coding</span>
+                            <span>production period: two days</span>
+                            <span>html5, css3, script</span>
+                        </div>
+                    </article>
+                    <article className='site_item s3'>
+                        <span className='num'>3.</span>
+                        <div className='text'>
+                            <div>Make</div>
+                            <div>site compliant with</div>
+                            <div>팀프로젝트</div>
+                        </div>
+                        <h3 className='title'>팀프로젝트 사이트 제작</h3>
+                        <div className='btn'>
+                            <a href="/">code</a>
+                            <a href="/">view</a>
+                        </div>
+                        <div className="infor">
+                            <span>site coding</span>
+                            <span>production period: two days</span>
+                            <span>html5, css3, script</span>
+                        </div>
+                    </article>
+                    <article className='site_item s4'>
+                        <span className='num'>4.</span>
+                        <div className='text'>
+                            <div>Make</div>
+                            <div>site compliant with</div>
+                            <div>2차 포트폴리오</div>
+                        </div>
+                        <h3 className='title'>2차포트폴리오 사이트 제작</h3>
+                        <div className='btn'>
+                            <a href="/">code</a>
+                            <a href="/">view</a>
+                        </div>
+                        <div className="infor">
+                            <span>site coding</span>
+                            <span>production period: two days</span>
+                            <span>html5, css3, script</span>
+                        </div>
+                    </article> */}
+                </section>
+            </div>
+        </section>
     )
 }
 
